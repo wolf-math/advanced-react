e599e4f3cbc5aff1abd828a02df8ca45298b5e64
aaron <aaron.wolf@gmail.com>
2021-02-22 17:35:25 +0200

    fixed styled components flicker on server render

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,5 +1,14 @@
 import PropTypes from 'prop-types';
+import Nprogress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
