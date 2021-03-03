c7d457b23e749a6af15065e7eb7c3a013f400ab1
aaron <aaron.wolf@gmail.com>
2021-03-03 08:56:36 +0200

    Setting up client side apollo

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,24 +1,35 @@
 import PropTypes from 'prop-types';
 import Nprogress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
 
 import 'nprogress/nprogress.css';
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
+// import { Component } from 'react';
 
 Router.events.on('routeChangeStart', () => Nprogress.start());
 Router.events.on('routeChangeComplete', () => Nprogress.done());
 Router.events.on('routeChangeError', () => Nprogress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.any,
-  pageProps: PropTypes.any,
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
 };
+
+export default withData(MyApp);
