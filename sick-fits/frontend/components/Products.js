ed51ce49b2c1baf5d9261ebd909f5df774cbdcd4
aaron <aaron.wolf@gmail.com>
2021-03-08 12:25:02 +0200

    front end styling and money format

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -1,5 +1,7 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import styled, { createGlobalStyle } from 'styled-components';
+import Product from './Product';
 
 const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
@@ -18,15 +20,21 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   console.log(data, error, loading);
   if (loading) return <p>Loading...</p>;
   return (
-    <div>
+    <ProductsListStyles>
       {data.allProducts.map((product) => (
-        <p key={product.id}>{product.name}</p>
+        <Product key={product.id} product={product} />
       ))}
-    </div>
+    </ProductsListStyles>
   );
 }
