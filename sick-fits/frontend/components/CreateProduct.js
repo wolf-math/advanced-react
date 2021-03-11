e112e198b2c36757d19230285f589d9c2c997f84
aaron <aaron.wolf@gmail.com>
2021-03-11 09:26:45 +0200

    form control

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -3,16 +3,28 @@ import Form from './styles/Form';
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'name',
     price: 123,
     description: 'neat',
   });
   return (
-    <Form>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(inputs);
+      }}
+    >
       <fieldset>
         <label htmlFor="image">
           Image
-          <input type="file" id="image" name="image" onChange={handleChange} />
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
         </label>
         <label htmlFor="name">
           name
@@ -33,6 +45,16 @@ export default function CreateProduct() {
             name="price"
             placeholder="price"
             value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
             onChange={handleChange}
           />
         </label>
--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -10,12 +10,12 @@ export default function useForm(initial = []) {
       value = parseInt(value);
     }
     if (type === 'file') {
-      value = e.target.files;
+      [value] = e.target.files;
     }
     setInputs({
       //  copy existing state
       ...inputs,
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
   }
 
